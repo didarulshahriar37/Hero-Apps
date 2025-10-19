02adff1fbd62365c322ad5cb55db2f9b3a6e8a6a
Didarul Shahriar <didarul13579@gmail.com>
2025-10-19 22:51:23 +0600

    added live site link

---
+++ README.md
@@ -0,0 +1,2 @@
+## Live Site Link:
+[Hero.io]('https://hero-io-d7.netlify.app/')
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const location = useLocation();
 
     return (
-        <div className="navbar bg-base-100 shadow-sm md:px-20 px-5">
+        <div className="navbar bg-base-100 shadow-sm md:px-20 px-2">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
